b3de15ba5d1ac71ff3e5a3740664118e87762e02
Matheus <davie.schimidtfonseca@gmail.com>
2023-04-05 09:56:47 -0300

    .
    .

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
