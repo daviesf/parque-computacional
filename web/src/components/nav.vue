b1d0065f1cfd5ffc77ce1ab5da3757aafe59a5f7
SamukaCode <samuelpcamargo2005@gmail.com>
2023-04-17 20:12:43 -0300

    .

--- web/src/components/nav.vue
+++ web/src/components/nav.vue
@@ -56,36 +56,35 @@
 </template>
 
 <script>
-import { routerLink } from 'vue-router'
+import VueRouter from 'vue-router'
 
 export default {
   name: 'NavBar', 
   directives: {
-    'router-link': routerLink,
+    'router-link': VueRouter.routerLink,
   },
-};
-
-document.addEventListener('DOMContentLoaded', function() {
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-closeBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    menuBtnChange();//calling the function(optional)
-})
-searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
-})
-// following are the code to change sidebar button(optional)
-function menuBtnChange() {
-    if (sidebar.classList.contains("open")) {
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-    } else {
-        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the iocns class
+  mounted() {
+    let sidebar = document.querySelector(".sidebar");
+    let closeBtn = document.querySelector("#btn");
+    let searchBtn = document.querySelector(".bx-search");
+    closeBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+        menuBtnChange();//calling the function(optional)
+    })
+    searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+        sidebar.classList.toggle("open");
+        menuBtnChange(); //calling the function(optional)
+    })
+    // following are the code to change sidebar button(optional)
+    function menuBtnChange() {
+        if (sidebar.classList.contains("open")) {
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+        } else {
+            closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the iocns class
+        }
     }
-}
-});
+  }
+};
 </script>
 
 <style src="../assets/home.css"></style>
