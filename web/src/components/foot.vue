5c2f7bea762b70c22569b0c9a6e4fe67176e96d4
SamukaCode <samuelpcamargo2005@gmail.com>
2023-05-16 19:47:29 -0300

    Update foot.vue

--- web/src/components/foot.vue
+++ web/src/components/foot.vue
@@ -2,12 +2,12 @@
   <div>
     <!-- Conteúdo principal da página -->
 
-    <footer class="rodape">
+    <footer class="rodape rodape-segment">
       <div class="ui inverted vertical footer segment rodape-segment">
         <div class="ui container">
           <div class="ui stackable inverted divided equal height stackable grid">
             <div class="sixteen wide column">
-              <p class="ui inverted center aligned segment">
+              <p class="ui inverted center aligned segment rodape-segment">
                 Secretaria de Administração Regional - Limeira (SP) | 2023 © Todos os direitos reservados <br />
                 Desenvolvido por Davie, Júnior, Neto e Samuel
               </p>
@@ -36,12 +36,11 @@ html, body {
   position: fixed;
   bottom: 0;
   width: 100%;
-  background: #4c41c2;
   padding-top: 10px;
   z-index: -9999; /* Valor do z-index para garantir sobreposição */
 }
 
 .rodape-segment {
-  background: #4c41c2;
+  background: #11101d !important; 
 }
 </style>
