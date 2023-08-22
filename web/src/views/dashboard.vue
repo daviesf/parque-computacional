4a248ae34222a3d1cbdd92107b9a4db85e3ff5be
NelsonLuisModenezJunior <juninhomodenez@hotmail.com>
2023-08-21 21:43:05 -0300

    Silencio seu...
    complete com sua criatividade

--- web/src/views/dashboard.vue
+++ web/src/views/dashboard.vue
@@ -2,6 +2,9 @@
   <div class="home-section">
     <h1>DashBoard informativo PowerBI</h1>
   </div>
+  <div>
+    <iframe title="Report Section" width="100%" height="680px" src="https://app.powerbi.com/view?r=eyJrIjoiMDZmMjRjYWUtMzA2Ny00ZGFmLTk4YzctMTBhZDM0OTI3OTU3IiwidCI6ImI0NzQxYTgyLTZiNmUtNDNhNS1hZDZlLTEwNDQ1MTFhYWVkNiJ9" frameborder="0" allowFullScreen="true"></iframe>
+  </div>
 
   <div class="ui container">
     <div class="ui grid centered">
