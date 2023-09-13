72759cc5cd5954969306bc037c2e93263b794468
Gonçalo Henrique <99834482+NelsonModenezNeto@users.noreply.github.com>
2023-09-13 11:52:20 -0300

    Atualizações HelpDeskUser
    Atualizações HelpDeskUser

--- web/src/views/helpDeskUser.vue
+++ web/src/views/helpDeskUser.vue
@@ -1,135 +1,128 @@
 <template>
-  <div>
-    <div class="textbox">
-      <div class="home_title_helpdesk">
-        <div id="saude" class="hello">Olá, [nome]</div>
-        <div class="welcome">Seja bem-vindo novamente!</div>
-      </div>
-    </div>
+  <div class="container">
 
-    <div class="ui container" style="margin-top: 20px">
-      <span class="text titlep">
-        <h1>Sistema de HelpDesk</h1>
-      </span>
-      <div class="ui two column grid" style="margin-top: 10px">
-        <div class="columnHelpDesk">
-          <div class="ui segment">
-            <h3 class="ui header">Novo Chamado</h3>
-            <form id="form" class="ui form">
-              <!-- <div class="field">
-                <label>Nome</label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Nome completo"
-                  class="campo required"
-                  id="nome"
-                  @input="nomeValidate"
-                />
-                <span class="span-required">Insira o Nome</span>
-              </div>
-              <div class="field">
-                <label>E-mail</label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="E-mail"
-                  class="campo required"
-                  id="email"
-                  @input="emailValidate"
-                />
-                <span class="span-required">Insira um e-mail válido</span>
-              </div> -->
-              <div class="field">
-                <label>Assunto</label>
-                <input
-                  type="text"
-                  name="subject"
-                  placeholder="Assunto"
-                  class="campo required"
-                  id="assunto"
-                  @input="assuntoValidate"
-                />
-                <span class="span-required">Insira o Assunto</span>
-              </div>
-              <!-- <div class="field">
-                <label>Prioridade</label>
-                <select
-                  name="prioridade"
-                  class="campo required"
-                  id="prioridade"
-                  @change="prioridadeValidate"
-                >
-                  <option class="placeholder" disabled selected>Selecione a prioridade</option>
-                  <option value="Alta">Alta</option>
-                  <option value="Média">Média</option>
-                  <option value="Baixa">Baixa</option>
-                </select>
-                <span class="span-required">Selecione a Prioridade</span>
-              </div> -->
-              <div class="field">
-                <label>Descrição</label>
-                <textarea
-                  name="description"
-                  rows="2"
-                  class="campo auto-expand required"
-                  id="descricao"
-                  @input="descricaoValidate"
-                ></textarea>
-                <span class="span-required">Insira a Descrição</span>
-              </div>
-              <div class="field">
-                <label>Arquivo</label>
-                <input type="file" name="arquivo" class="campo required" id="arquivo" />
-                <span class="span-required">Insira o Arquivo</span>
-              </div>
-              <button type="submit" class="ui button" id="enviarChamado">Enviar</button>
-              <button class="ui button" type="button" onclick="limparFormulario()">
-                Limpar Formulário
-              </button>
-            </form>
-          </div>
+    <div class="ui grid" style="height: 101.5vh;">
+      <div class="six wide column navhelp">
+        <div class="home_title" style="color: white;">
+          <div id="saude" class="hello1">Olá, [nome]</div>
+          <div class="welcome1">Seja bem-vindo novamente!</div>
         </div>
-        <div class="columnHelpDesk">
-          <div class="ui segment">
-            <h3 class="ui header">Meus Chamados</h3>
-            <table class="ui celled table">
-              <thead>
-                <tr>
-                  <th>Nome</th>
-                  <th>E-mail</th>
-                  <th>Assunto</th>
-                  <th>Prioridade</th>
-                  <th>Descrição</th>
-                  <th>Arquivo</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody id="tableBody">
-                <tr>
-                  <td>Davie</td>
-                  <td>cl201275@g.unicamp.br</td>
-                  <td>Problema</td>
-                  <td>Alta</td>
-                  <td class="td-desc"><button type="submit" class="ui button desc">Ver</button></td>
-                  <td class="td-desc"><button type="submit" class="ui button desc">Ver</button></td>
-                  <td>Pendente</td>
-                </tr>
-              </tbody>
-            </table>
+      </div>
+      <div class="ten wide column">
+        <span class="text titlep">
+          <h1 style="margin-top: 30px;">Sistema de HelpDesk</h1>
+        </span>
+
+          <div class="columnHelpDesk" style="width: 80%;">
+            <div class="ui segment">
+              <h3 class="ui header">Novo Chamado</h3>
+              <form id="form" class="ui form">
+                <!-- <div class="field">
+                    <label>Nome</label>
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Nome completo"
+                      class="campo required"
+                      id="nome"
+                      @input="nomeValidate"
+                    />
+                    <span class="span-required">Insira o Nome</span>
+                  </div>
+                  <div class="field">
+                    <label>E-mail</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="E-mail"
+                      class="campo required"
+                      id="email"
+                      @input="emailValidate"
+                    />
+                    <span class="span-required">Insira um e-mail válido</span>
+                  </div> -->
+                <div class="field">
+                  <label>Assunto</label>
+                  <input type="text" name="subject" placeholder="Assunto" class="campo required" id="assunto"
+                    @input="assuntoValidate" />
+                  <span class="span-required">Insira o Assunto</span>
+                </div>
+                <!-- <div class="field">
+                    <label>Prioridade</label>
+                    <select
+                      name="prioridade"
+                      class="campo required"
+                      id="prioridade"
+                      @change="prioridadeValidate"
+                    >
+                      <option class="placeholder" disabled selected>Selecione a prioridade</option>
+                      <option value="Alta">Alta</option>
+                      <option value="Média">Média</option>
+                      <option value="Baixa">Baixa</option>
+                    </select>
+                    <span class="span-required">Selecione a Prioridade</span>
+                  </div> -->
+                <div class="field">
+                  <label>Descrição</label>
+                  <textarea name="description" rows="2" class="campo auto-expand required" id="descricao"
+                    @input="descricaoValidate"></textarea>
+                  <span class="span-required">Insira a Descrição</span>
+                </div>
+                <div class="field">
+                  <label>Arquivo</label>
+                  <input type="file" name="arquivo" class="campo required" id="arquivo" />
+                  <span class="span-required">Insira o Arquivo</span>
+                </div>
+                <button type="submit" class="ui button" id="enviarChamado">Enviar</button>
+                <button class="ui button" type="button" onclick="limparFormulario()">
+                  Limpar Formulário
+                </button>
+              </form>
+            </div>
+          </div>
+
+          <div class="columnHelpDesk" style="width: 80%;">
+            <div class="ui segment">
+              <h3 class="ui header">Meus Chamados</h3>
+              <table class="ui celled table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Assunto</th>
+                    <th>Prioridade</th>
+                    <th>Descrição</th>
+                    <th>Arquivo</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody id="tableBody">
+                  <tr>
+                    <td>Davie</td>
+                    <td>cl201275@g.unicamp.br</td>
+                    <td>Problema</td>
+                    <td>Alta</td>
+                    <td class="td-desc"><button type="submit" class="ui button desc">Ver</button></td>
+                    <td class="td-desc"><button type="submit" class="ui button desc">Ver</button></td>
+                    <td>Pendente</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
+
+  </div>
+
+  <div class="logout-button">
+    <router-link to="/logout">
+      <i class="sign out icon hpu"></i>
+    </router-link>
+    <div class="logout-display">
+      <p>Logout</p>
     </div>
 
-    <div class="logout-button">
-      <router-link to="/logout">
-        <i class="sign out icon hpu"></i>
-      </router-link>
-      <div class="logout-display">
-        <p>Logout</p>
-      </div>
-    </div>
   </div>
 </template>
 
